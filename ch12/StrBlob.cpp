77d28229a5117d25871440d2db0209476f89e6c7
hys2015 <markheng2014@gmail.com>
2016-08-10 22:54:37 +0800

    finish new and delete execise

--- ch12/StrBlob.cpp
+++ ch12/StrBlob.cpp
@@ -29,3 +29,15 @@ void StrBlob::pop_back()
     check(0, "pop_back on empty StrBlob");
     data->pop_back();
 }
+// testing
+// main function
+int StrBlobTest()
+{
+    StrBlob blob1;
+    StrBlob blob2 = { "a", "b", "c" };
+    blob1 = blob2;
+    blob2.push_back("ddd");
+    //blob1.data {"a", "b", "c", "ddd"}
+    //blob2.data is the same as blob1's
+    return 0;
+}
--- ch12/main.cpp
+++ ch12/main.cpp
@@ -1,15 +1,12 @@
-#include "Strblob.h"
 
 #include <vector>
 #include <string>
 
+#define MAIN_FUNC exec12_7_main();
+
+extern int MAIN_FUNC;
+
 int main()
 {
-    StrBlob blob1;
-    StrBlob blob2 = { "a", "b", "c" };
-    blob1 = blob2;
-    blob2.push_back("ddd");
-    //blob1.data {"a", "b", "c", "ddd"}
-    //blob2.data is the same as blob1's
-    return 0;
+    return MAIN_FUNC;
 }
