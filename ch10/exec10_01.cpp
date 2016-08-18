89b472df07e21b0eba8fdfeb639790f3ab0874d2
hys2015 <markheng2014@gmail.com>
2016-08-18 17:06:46 +0800

    chapter10-1

---
+++ ch10/exec10_01.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+
+#include <vector>
+#include <iostream>
+
+int exec10_01_main()
+{
+    std::vector<int> vec;
+    int in;
+    std::cout << "set your vector of ints(-1 means ending): ";
+    while (std::cin >> in)
+    {
+        if (in == -1) break;
+        vec.push_back(in);
+    }
+    std::cout << "enter your int: ";
+    std::cin >> in;
+    std::cout << in << " count is "
+              << count(vec.cbegin(), vec.cend(), in) << std::endl;
+
+    return 0;
+}
---
+++ ch10/exec10_02.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <string>
+#include <list>
+
+#include <iostream>
+
+int exec10_02_main()
+{
+    std::list<std::string> list;
+    std::string in;
+    std::cout << "set your vector of strings(q means ending): ";
+    while (std::cin >> in)
+    {
+        if (in == "q") break;
+        list.push_back(in);
+    }
+    std::cout << "enter your string: ";
+    std::cin >> in;
+    std::cout << in << " count is "
+        << count(list.cbegin(), list.cend(), in) << std::endl;
+
+    return 0;
+}
